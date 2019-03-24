b9c36ea90d117d0ac57502b0040cd01002b8c6de
akshattrivedi <akshat.trivedi08@gmail.com>
2019-03-25 02:19:02 +0530

    Added Static Files

--- it_sales/settings.py
+++ it_sales/settings.py
@@ -121,6 +121,9 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 
 STATIC_URL = '/static/'
 
